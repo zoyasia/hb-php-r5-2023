32da03fb79e4318ee3db86ba253547083854d1b6
Lucas Delobelle <lucas@ld-web.net>
2023-07-26 15:27:48 +0200

    :tada: PHP intro
    Bases, variables, fonctions SPL (echo, var_dump), commentaires...
    Avant les tableaux

---
+++ intro/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coucou</title>
+</head>
+
+<body>
+  <h1><?php echo "Coucou"; ?></h1>
+  <?php
+  const SOFTWARE_VERSION = "1.0.0";
+  $age = 45;
+  $name = "Bobby";
+
+  echo 'Bonjour $name, vous avez $age ans<br />';
+  echo "Bonjour $name, vous avez $age ans";
+
+  var_dump($age);
+  var_dump($name);
+  ?>
+</body>
+
+</html>
