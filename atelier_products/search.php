2e5146641d8c5ec3b79a10b2ead51cf43958f131
Zoé_Dazy <zoe.dazy+github@gmail.com~>
2023-08-02 11:29:22 +0200

    ajout pull lucas

---
+++ atelier_products/search.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <a href="stylesheet" a="assets/styles.css"></a>
+    <title>Search Bar</title>
+</head>
+<body>
+    <form action="" method="get">
+      <label for="search_bar">Recherche</label>
+      <input type="text" id="search" name="search_bar" />
+    </form>
+</body>
+</html>
