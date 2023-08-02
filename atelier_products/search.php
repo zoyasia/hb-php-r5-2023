98e4f5335cf84b936107d0e57795f95987e7eba3
Zoé_Dazy <zoe.dazy+github@gmail.com~>
2023-08-02 14:19:39 +0200

    search bar

--- atelier_products/search.php
+++ atelier_products/search.php
@@ -1,3 +1,8 @@
+<?php
+require_once 'data/products.php';
+require_once 'layout/header.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,9 +12,48 @@
     <title>Search Bar</title>
 </head>
 <body>
-    <form action="" method="get">
-      <label for="search_bar">Recherche</label>
-      <input type="text" id="search" name="search_bar" />
+<h1>RECHERCHE</h1>
+
+    <form action="" method="GET">
+      <label for="search">Recherche</label>
+      <input type="text" id="search" name="search" placeholder="Entrez votre recherche"/>
+      <button type="submit">Envoyer</button>
     </form>
 </body>
 </html>
+
+<?php
+var_dump($_GET);
+if (isset($_GET['search']))
+{
+    ['search' => $search] = $_GET;
+
+    $results = array_filter($products, fn ($el) => str_contains($el['name'], $search));
+
+    if ($results === false)
+    {
+        echo "Aucun résultat trouvé";
+    }
+    else
+    {
+        var_dump($results);
+    }
+}
+
+
+
+
+//    $productsNames = array_column($products, 'name');
+//    $nameKey = array_search($search, $productsNames);
+
+//    if ($nameKey === false) {
+//        echo "Aucun résultat trouvé";
+//    } else {
+//        var_dump($products[$nameKey]);
+//    }
+//}
+
+require_once 'layout/footer.php';
+
+
+
